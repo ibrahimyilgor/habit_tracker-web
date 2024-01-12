222ccc69d7b481696b605ee7c942181100e25055
İbrahim Yılgör <yilgor16@itu.edu.tr>
2024-01-12 19:27:43 +0300

    fix: branchMenu to branchmenu

--- client/src/layouts/dashboard/layout.js
+++ client/src/layouts/dashboard/layout.js
@@ -1,9 +1,9 @@
 import { useCallback, useEffect, useState } from "react";
-import { usePathname } from "next/navigation";
 import { styled } from "@mui/material/styles";
 import { withAuthGuard } from "src/hocs/with-auth-guard";
 import { SideNav } from "./side-nav";
 import { TopNav } from "./top-nav";
+import { useRouter } from "next/router";
 
 const SIDE_NAV_WIDTH = 280;
 
@@ -25,10 +25,13 @@ const LayoutContainer = styled("div")({
 
 export const Layout = withAuthGuard((props) => {
   const { children } = props;
-  let pathname = window.location.href;
+  const router = useRouter();
+  const pathname = router.pathname;
   const [openNav, setOpenNav] = useState(false);
 
   const handlePathnameChange = useCallback(() => {
+    console.log("PATHNAME", pathname);
+
     if (openNav) {
       setOpenNav(false);
     }
